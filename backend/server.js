721d362247b1eec93e57b751bc73b7f7136f6c19
KATTELA NANI <kattelanani64@gmail.com>
2026-08-18 20:05:14 +0530

    Fix duplicate PORT declaration

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 app.set("trust proxy", true);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     const ip = req.ip;
